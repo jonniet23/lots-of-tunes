0be0f935ffd4559711c2c6dce2fb53caf55c19d7
Jonnie <jonniet23@hotmail.com>
2023-07-27 22:00:07 +0100

    Reading single artist test complete

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -23,5 +23,21 @@ const getAllArtists = async (_, res) => {
     res.status(500).json(err.message);
   }
 };
+const getSingleArtist = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const {
+      rows: [artist],
+    } = await db.query("SELECT * FROM Artists WHERE id = $1", [id]);
 
-module.exports = { createArtist, getAllArtists };
+    if (!artist) {
+      return res.status(404).json({ message: `artist ${id} does not exist` });
+    }
+
+    res.status(200).json(artist);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+module.exports = { createArtist, getAllArtists, getSingleArtist };
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { createArtist, getAllArtists } = require("../controllers/artist");
+const {
+  createArtist,
+  getAllArtists,
+  getSingleArtist,
+} = require("../controllers/artist");
 
 const artistRouter = express.Router();
 
 artistRouter.post("/", createArtist);
 artistRouter.get("/", getAllArtists);
+artistRouter.get("/:id", getSingleArtist);
+
 module.exports = artistRouter;
