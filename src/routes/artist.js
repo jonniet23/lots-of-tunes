77884330f63e4e81c7d43f0767e5410927db4e13
Jonnie <jonniet23@hotmail.com>
2023-07-30 21:54:26 +0100

    delete and update artists tests completed

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -3,6 +3,8 @@ const {
   createArtist,
   getAllArtists,
   getSingleArtist,
+  updateArtist,
+  deleteArtist,
 } = require("../controllers/artist");
 
 const artistRouter = express.Router();
@@ -10,5 +12,6 @@ const artistRouter = express.Router();
 artistRouter.post("/", createArtist);
 artistRouter.get("/", getAllArtists);
 artistRouter.get("/:id", getSingleArtist);
-
+artistRouter.patch("/:id", updateArtist);
+artistRouter.delete("/:id", deleteArtist);
 module.exports = artistRouter;
